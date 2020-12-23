7da39efabe819288141ceb982d6574e9efffe978
Sergey A Volkov <serg-v@yandex-team.ru>
2020-12-23 20:19:30 +0300

    Copy pasted show usages

--- src/main/kotlin/com/github/volkov/znavi/PopupAction.kt
+++ src/main/kotlin/com/github/volkov/znavi/PopupAction.kt
@@ -1,16 +1,10 @@
 package com.github.volkov.znavi
 
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.ui.popup.JBPopupFactory
+import com.intellij.find.actions.ShowUsagesAction
 
 /**
  * User: serg-v
  * Date: 12/19/20
  * Time: 9:05 AM
  */
-class PopupAction : AnAction() {
-    override fun actionPerformed(e: AnActionEvent) {
-        JBPopupFactory.getInstance().createPopupChooserBuilder(listOf("a", "b", "c")).createPopup().showInFocusCenter()
-    }
-}
+class PopupAction : ShowUsagesAction()
