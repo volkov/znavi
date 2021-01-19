ebe6c3071dad622d93c4e9029f44b3e81406effc
Sergey A Volkov <serg-v@yandex-team.ru>
2021-01-19 21:39:00 +0300

    #9 navigate to method

--- src/main/kotlin/com/github/volkov/znavi/ZNaviGoToParameterDeclaration.kt
+++ src/main/kotlin/com/github/volkov/znavi/ZNaviGoToParameterDeclaration.kt
@@ -12,7 +12,7 @@ import com.intellij.pom.Navigatable
  * Date: 1/9/21
  * Time: 3:54 PM
  */
-class GoToParameterDeclaration : GotoDeclarationAction() {
+class ZNaviGoToParameterDeclaration : GotoDeclarationAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
         val editor = e.getData(CommonDataKeys.EDITOR)
@@ -20,9 +20,10 @@ class GoToParameterDeclaration : GotoDeclarationAction() {
         if (file != null && editor != null) {
 
             val targetElement = TargetElementUtil.getInstance()
-                    .findTargetElement(editor, TargetElementUtil.REFERENCED_ELEMENT_ACCEPTED, editor.caretModel.offset)
-            val element = targetElement!!.navigationElement
-            val navigatable = if (element is Navigatable) element else EditSourceUtil.getDescriptor(element)
+                    .findTargetElement(editor, TargetElementUtil.getInstance().allAccepted, editor.caretModel.offset)
+            val navigationElement = targetElement!!.navigationElement
+            val trueNavigationElement = TargetElementUtil.getInstance().getGotoDeclarationTarget(targetElement, navigationElement)
+            val navigatable = if (trueNavigationElement is Navigatable) trueNavigationElement else EditSourceUtil.getDescriptor(trueNavigationElement)
             if (navigatable != null && navigatable.canNavigate()) {
                 navigatable.navigate(true)
             }
